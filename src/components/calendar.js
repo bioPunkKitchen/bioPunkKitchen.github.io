57e686f745858a717480365b6a55895bb1096d5f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-02-26 11:27:45 +0100

    kalender deutsch

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -4,7 +4,7 @@ const Calendar = () => (
   <div class="calendar">
     <iframe
       title="Kalender" // TODO: use configuration file
-      src="https://open-web-calendar.herokuapp.com/calendar.html?url=https%3A%2F%2Fwww.meetup.com%2Fbiopunx%2Fevents%2Fical%2F&url=https%3A%2F%2Fforum.wilap.de%2Fc%2Fkitchen%2Fl%2Fcalendar.ics%3Ftime_zone%3DEurope%2FBerlin">
+      src="https://open-web-calendar.herokuapp.com/calendar.html?url=https%3A%2F%2Fwww.meetup.com%2Fbiopunx%2Fevents%2Fical%2F&url=https%3A%2F%2Fforum.wilap.de%2Fc%2Fkitchen%2Fl%2Fcalendar.ics%3Ftime_zone%3DEurope%2FBerlin&language=de">
     </iframe>
   </div>
 )
