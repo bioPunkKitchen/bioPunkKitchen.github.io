da13ad70f0c9e5d579e984a5f4fd86863b9b7dee
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-02-26 11:41:56 +0100

    Kontaktmöglichkeiten beschrieben

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,11 +9,16 @@ const Footer = () => (
       marginTop: `1.45rem`,
     }}
     >
-    © {new Date().getFullYear()}
+    © {new Date().getFullYear() + ` `}
     <span class="license"
       dangerouslySetInnerHTML={{
         __html: `<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />Dieses Werk von <a href="https://github.com/bioPunkKitchen/bioPunkKitchen.github.io/graphs/contributors" xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">bioPunk.kitchen-Entwicklern</a> ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz</a> und beruht auf dem Werk unter <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/bioPunkKitchen/bioPunkKitchen.github.io/" rel="dct:source">https://github.com/bioPunkKitchen/bioPunkKitchen.github.io/</a>.` }}
       />
+    <ul>
+      <li>
+        <a href="https://machbar-potsdam.de/about/">Impressum</a>
+      </li>
+    </ul>
   </footer>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,21 @@ const IndexPage = () => (
     <Newsletter />
     <Map />
     <Calendar />
+    <div class="contact">
+      Um mit uns in Kontakt zu treten, gibt es mehrere Wege.
+      In der <a href="https://t.me/joinchat/EW_mnFTIFmguUpz0z7xfJA">Telegram-Gruppe</a> {` `}
+      chatten wir miteinander.
+      Im <a href="https://forum.wilap.de/c/kitchen">Forum</a> besprechen,
+      planen und koordinieren wir Projekte in der bioPunk.kitchen und
+      mit <a href="https://forum.wilap.de/">anderen Projekten</a> der {` `}
+      <a href="https://machbar-potsdam.de">machBar Potsdam</a>.
+      Du kannst der <a href="https://www.meetup.com/BiopunX/">Meetup-Gruppe</a>  {` `}
+      beitreten, um über Veranstaltungen und Treffen auf dem Laufenden zu bleiben.
+      Den <a href="javascript:showMailingPopUp()">Newsletter</a> kannst Du abbonieren,
+      wenige Male im Jahr eine Übersicht über die Projekte der bioPunk.kitchen
+      zu erhalten.
+
+    </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
